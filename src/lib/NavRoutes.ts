374bb9cebf1a56c84e4a14b09f88c9503ca78d72
Tanya Grace S. Gonzaga <tanyangqty25@gmail.com>
2024-11-23 09:37:32 +0800

    first commit

---
+++ src/lib/NavRoutes.ts
@@ -0,0 +1,24 @@
+const routes = [
+	{
+		href: '/',
+		label: 'Home'
+	},
+	{
+		href: '/dashboard',
+		label: 'Dashboard'
+	},
+	{
+		href: '/medicine-list',
+		label: 'Medicine List'
+	},
+	{
+		href: '/patient-list',
+		label: 'Patient List'
+	},
+	{
+		href: '/prescription',
+		label: 'Prescription'
+	}
+];
+
+export default routes;
